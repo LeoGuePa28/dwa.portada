a5d80f3e11ed68943e1c887f4e7f64654c8bcaa0
domin <domin@LAPTOP-PU5AQROO>
2021-05-20 17:27:26 -0500

    Cargo Inicial

---
+++ src/test/java/com/dwa/portada/WebPortadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.portada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
